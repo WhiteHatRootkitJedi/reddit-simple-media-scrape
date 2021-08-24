1a2b8410d7307ccea36cf0846dc8a4fcb536538a
David Novak <david.novak@legrand.us>
2021-08-24 18:30:29 -0400

    More stuff

--- reddit_scrape.py
+++ reddit_scrape.py
@@ -43,23 +43,31 @@ class RedditScrape(object):
 
             for post in posts:
                 countWithoutFail += 1
-		try:
-		    title = post['data']['title']
+                try:
+                    title = post['data']['title']
                     UTC_datetime_timestamp = float(post['data']['created_utc'])
                     local_datetime_converted = datetime.datetime.fromtimestamp(UTC_datetime_timestamp)
+                    filename = str(local_datetime_converted) + ' - ' + title + '.jpg'
+                    path = os.path.join(self.directory, filename)
 
-		    #sys.stdout.write('Title: ' + title + '\n\n')
+                    #sys.stdout.write('Title: ' + title + '\n\n')
                     errorUrl = post['data']['url']
                     url = post['data']['secure_media']['oembed']['thumbnail_url']
-		except (TypeError, KeyError) as e:
-		    #sys.stdout.write('Error: ' + str(e) + '\n')
-                    sys.stdout.write('<a href="' + errorUrl + '">' + title + '</a>\n')
-		    continue
+                except (TypeError, KeyError) as e:
+                    if errorUrl.find('/a/') != -1:
+                        #print('---------Succeeding: ' + errorUrl + '\n')
+                        downloadImgurAlbum(errorUrl[errorUrl.rindex('/')+1:], path)
+                    else:
+                        try:
+                            download('https://i.imgur.com/' + errorUrl[errorUrl.index('/'):] + '.jpg', path)
+                        except:
+                            sys.stdout.write('\n<a href="' + errorUrl + '">' + title + '</a>\n')
+
+                    continue
 
                 if re.search('(jpg|jpeg|png|gif)', url):
-
                     if url[len(url)-2:] == 'fb':
-                        url = url[:len(url)-2]
+                        url = url[:len(url)-3]
 
                     #filename = re.match('https://i.redd.it\/(\w+\.\w+)', url)
                     #if not filename:
@@ -74,25 +82,21 @@ class RedditScrape(object):
                     #if self.prefix:
                     #    filename = '{}-{}'.format(self.prefix, filename)
 
-		    filename = str(local_datetime_converted) + ' - ' + title + '.jpg'
+                    #filename = str(local_datetime_converted) + ' - ' + title + '.jpg'
                     #sys.stdout.write('\n' + filename + '\n')
 
                     if not self.dry_run:
-                        path = os.path.join(self.directory, filename)
+                        #path = os.path.join(self.directory, filename)
 
-                        # Check if file already exists
-                        if not os.path.isfile(path):
-                            urlretrieve(url, path)
-			else:
-                            path = os.path.join(self.directory, filename + str(random.randint(1000,9999)))
-                            urlretrieve(url, path)
+                        download(url, path)
 
-                        set_comment(path, errorUrl)
+                        #set_comment(path, errorUrl)
 
-                    #sys.stdout.write('.')
-                    #sys.stdout.flush()
-                    count += 1
-
+                        #sys.stdout.write('.')
+                        #sys.stdout.flush()
+                        count += 1
+                else:
+                    sys.stdout.write('\n<a href="' + errorUrl + '">' + title + '</a>\n')
             if payload['data']['after']:
                 user_url = self.PROFILE_URL.format(self.username) + '?after={}'.format(payload['data']['after'])
             else:
@@ -115,10 +119,42 @@ def main():
     scraper = RedditScrape(args.username, args.directory, args.dry_run, args.prefix)
     scraper.scrape()
 
-def set_comment(file_path, comment_text):
-    import py_applescript
-    applescript.tell.app("Finder", 'set comment of (POSIX file "{}" as alias) to "{}" as Unicode text'.format(file_path, comment_text))
+def downloadImgurAlbum(hash, path):
+    try:
+        req = Request('https://api.imgur.com/3/album/' + hash)
+        req.add_header('Authorization', 'Client-ID a5a67e97ced5de3')
+        data = urlopen(req).read().decode()
+        albumJson = json.loads(data)
 
+        for image in albumJson['data']['images']:
+            link = json.dumps(image['link'])[1:-1]
+            #sys.stdout.write(link + '\n')
+            #filename = dateString + ' - ' + json.dumps(albumJson['data']['title'])[1:-1] + '.jpg'
+
+            download(link, path)
+
+    #else:
+        #path = os.path.join(self.directory, filename[0:-4] + str(random.randint(1000,9999)) + '.jpg')
+        #urlretrieve(url, path)
+#except:
+    except:
+        #print('failed to get album')
+        pass
+
+#curl --location -g --request GET 'https://api.imgur.com/3/album/TAOoFD3' \
+#--header 'Authorization: Client-ID a5a67e97ced5de3'
+
+#def set_comment(file_path, comment_text):
+    #import py_applescript
+    #applescript.tell.app("Finder", 'set comment of (POSIX file "{}" as alias) to "{}" as Unicode text'.format(file_path, comment_text))
+
+def download(url, path):
+    # Check if file already exists
+    if os.path.isfile(path):
+        path = os.path.join(self.directory, filename[0:-4] + ' - ' + str(random.randint(100,999)) + '.jpg')
+
+    urlretrieve(url, path)
+    sys.out.write('.')
 
 if __name__ == '__main__':
     main()
